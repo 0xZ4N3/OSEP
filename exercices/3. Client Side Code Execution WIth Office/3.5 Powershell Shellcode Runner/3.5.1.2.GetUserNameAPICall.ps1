9ecf8ac8d042a15b813aeb7c3c3a3bd190a477ef
0xZ4N3 <z4n3@outlook.es>
2025-03-03 16:42:19 +0100

    Organization changed

---
+++ exercices/3. Client Side Code Execution WIth Office/3.5 Powershell Shellcode Runner/3.5.1.2.GetUserNameAPICall.ps1
@@ -0,0 +1,30 @@
+﻿$Advapi32 = @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Advapi32 {
+    [DllImport("advapi32.dll", SetLastError = true)]
+    public static extern bool GetUserName(System.Text.StringBuilder sb, ref Int32 length);
+}
+"@
+
+Add-Type $Advapi32
+
+$User32 = @"
+using System;
+using System.Runtime.InteropServices;
+
+public class User32 {
+    [DllImport("user32.dll", CharSet=CharSet.Auto)]
+    public static extern int MessageBox(IntPtr hWnd, String text, 
+        String caption, int options);
+}
+"@
+
+Add-Type $User32
+
+$buffer = New-Object System.Text.StringBuilder
+$strlen = 64
+
+$tmp = [Advapi32]::GetUserName($buffer,[ref]$strlen)
+$tmp = [User32]::MessageBox(0, $buffer, "Username", 0)
---
+++ exercices/3. Client Side Code Execution WIth Office/3.6 Keep that in PowerShell Memory/LookupFunc.ps1
@@ -0,0 +1,14 @@
+﻿function LookupFunc {
+
+	Param ($moduleName, $functionName)
+
+	$assem = ([AppDomain]::CurrentDomain.GetAssemblies() | 
+    Where-Object { $_.GlobalAssemblyCache -And $_.Location.Split('\\')[-1].
+      Equals('System.dll') }).GetType('Microsoft.Win32.UnsafeNativeMethods')
+    $tmp=@()
+    $assem.GetMethods() | ForEach-Object {If($_.Name -eq "GetProcAddress") {$tmp+=$_}}
+    $test = @(($assem.GetMethod('GetModuleHandle')).Invoke($null, @($moduleName)), $functionName)
+	return $tmp[1].Invoke($null, $test)
+}
+
+LookupFunc "user32.dll" "MessageBoxA"
